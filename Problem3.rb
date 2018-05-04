26a9e00660a02c150bc971df4c0860271337b8a7
anametiu <38921888+anametiu@users.noreply.github.com>
2018-05-04 19:03:51 +0300

    Problem3 red

---
+++ Problem3.rb
@@ -0,0 +1,42 @@
+require 'rspec'
+
+def contains_duplicates?(array)
+
+  min = 0
+
+  array.each_index do |i|
+
+    temp = array[i]
+    j = temp - min
+
+    while i != j do
+
+      if temp == array[j]
+        return true
+      end
+
+      array[i] = array[j]
+      array[j] = temp
+      temp = array[i]
+
+      j = temp - min
+
+    end
+
+  end
+
+  return false
+
+end
+
+describe 'Problem3' do
+
+  it 'should find duplicates' do
+
+    puts contains_duplicates?([1, 2, 3, 4, 2])
+    puts contains_duplicates?([1, 2, 6, 6, 2, 3])
+    puts contains_duplicates?([1, 2, 3])
+    puts contains_duplicates?([1, 1, 1])
+
+  end
+end
