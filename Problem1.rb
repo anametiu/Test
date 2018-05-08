d2d8a7489a6358e37802db6ee26054408c72efad
anametiu <38921888+anametiu@users.noreply.github.com>
2018-05-08 21:29:51 +0300

    Updated problem1 according to required changes
    Implemented algorithm in one line without auxiliary variable
    Removed empty lines
    Removed return

--- Problem1.rb
+++ Problem1.rb
@@ -1,18 +1,7 @@
-def  sum_even_numbers(array)
+class Problem1
 
-  sum = 0
-  array.each { |x| sum += x.even? ? x : 0 }
-  return sum
-
-end
-
-describe 'Problem1' do
-
-  it 'should sum even numbers' do
-
-    puts expect(sum_even_numbers([1, 2, 3, 4, 5, 6])).to eq(12)
-    puts expect(sum_even_numbers([3, 2, 7, 1, 1, 1])).to eq(2)
-    puts expect(sum_even_numbers([2, 6, 2, 4, 2, 2])).to eq(18)
-
+  def  sum_even_numbers(array)
+    array.select(&:even?).sum
   end
+
 end
