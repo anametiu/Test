1ff3258083816dee642b536ef7b4c8e256013188
anametiu <38921888+anametiu@users.noreply.github.com>
2018-05-13 11:16:38 +0300

    Update test_problem3.rb

--- test_problem3.rb
+++ test_problem3.rb
@@ -1,7 +1,7 @@
 require_relative "src/problem3"
 require "test/unit"
 
-class TestProblem13< Test::Unit::TestCase
+class TestProblem3< Test::Unit::TestCase
 
   def test_simple
     assert_true(Problem3.new.contains_duplicates?([1, 2, 3, 4, 2]))
